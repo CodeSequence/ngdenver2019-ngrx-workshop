31ff4641360428321488fda805e6b5ae30a0b452
Brandon Roberts <robertsbt@gmail.com>
2019-07-29 21:49:12 -0600

    chore: update to version 8

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from "@angular/core";
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatSidenavModule,
-  MatToolbarModule
-} from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   imports: [
